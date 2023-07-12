091fe1e9c20a69842a7bc979141856a627473458
koppai <psthu.koppai@gmail.com>
2023-07-12 22:29:22 +0630

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/navBar';
+import Categories from './pages/categories';
+import BookPage from './pages/bookPage';
 
 function App() {
   return (
     <div className="App">
-      hi
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<BookPage />}
+        />
+        <Route
+          path="/categories"
+          element={<Categories />}
+        />
+      </Routes>
     </div>
   );
 }
