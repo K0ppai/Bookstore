ed0b7c6e857f4757fea2839a7b2dff566cddf58e
koppai <psthu.koppai@gmail.com>
2023-07-18 11:50:41 +0630

    Create `slice` state for books

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      const bookId = action.payload;
+      state.books = state.books.filter((book) => book.id !== bookId);
+    },
+  },
+});
+
+export default bookSlice.reducer;
