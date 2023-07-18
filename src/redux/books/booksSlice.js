2fd762e0b712f3c6ca6c5a277e275a83769438c9
koppai <psthu.koppai@gmail.com>
2023-07-18 21:41:54 +0630

    Use v4 as id for the item_id

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 as id } from 'uuid';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: id(),
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: id(),
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: id(),
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const bookSlice = createSlice({
@@ -13,7 +33,7 @@ const bookSlice = createSlice({
     },
     removeBook: (state, action) => {
       const bookId = action.payload;
-      state.books = state.books.filter((book) => book.id !== bookId);
+      state.books = state.books.filter((book) => book.item_id !== bookId);
     },
   },
 });
